f380a7ce5a501ad9fcb02578ac83754a136a628d
Famouzcoder <olowookerefaith@gmail.com>
2024-08-17 19:51:02 +0100

    Implement PlaidLink and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,81 +1,85 @@
-'use client'
+'use client';
+
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
-
 
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import CustomInput from './CustomInput';
+import { authFormSchema } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import CustomInput from './CustomInput'
-import { authFormSchema } from '@/lib/utils'
-import { Loader2 } from 'lucide-react'
-import { types } from 'util'
-import { signUp, signIn, getLoggedInUser } from '@/lib/actions/user.actions'
-
-
-
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
-    const router = useRouter();
-    const [user, setUser] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const formSchema = authFormSchema(type);
+  const formSchema = authFormSchema(type);
 
     // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            email: "",
-            password: ''
-        },
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        email: "",
+        password: ''
+      },
     })
 
     // 2. Define a submit handler.
     const onSubmit = async (data: z.infer<typeof formSchema>) => {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        setIsLoading(true);
+      setIsLoading(true);
 
-        try{
-        //sign up with appwrite and create plaid token
+      try {
+        // Sign up with Appwrite & create plaid token
 
-        if (type ==='sign-up'){
-            const newUser = await signUp(data);
+        if(type === 'sign-up') {
+          const userData = {
+            firstName: data.firstName!,
+            lastName: data.lastName!,
+            address1: data.address1!,
+            city: data.city!,
+            state: data.state!,
+            postalCode: data.postalCode!,
+            dateOfBirth: data.dateOfBirth!,
+            ssn: data.ssn!,
+            email: data.email,
+            password: data.password
+          }
 
-            setUser(newUser)
-            }
+          const newUser = await signUp(userData);
 
-        if (type=== 'sign-in'){
-            const response = await signIn({
-                email: data.email,
-                password: data.password,
-            })
-            if(response) router.push('/')
-
+          setUser(newUser);
         }
-            // console.log(values)
-            // setIsLoading(false);
-        }
-        catch(error){
-            console.log(error)
 
-        }finally{
-            setIsLoading(false);
+        if(type === 'sign-in') {
+          const response = await signIn({
+            email: data.email,
+            password: data.password,
+          })
+
+          if(response) router.push('/')
         }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
     return (
         <section className='auth-form'>
@@ -92,29 +96,29 @@ const AuthForm = ({ type }: { type: string }) => {
                         Famouz Bank
                     </h1>
                 </Link>
-                <div className='flex flex-col gap-1 md:gap-3'>
-                    <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
+                <div className="flex flex-col gap-1 md:gap-3">
+                        <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
                         {user
                             ? 'Link Account'
                             : type === 'sign-in'
-                                ? 'Sign In'
-                                : 'Sign Up'
+                            ? 'Sign In'
+                            : 'Sign Up'
                         }
-                        <p className='text-16 font-normal text-gray-600'>
+                        <p className="text-16 font-normal text-gray-600">
                             {user
-                                ? 'Link your account to get started'
-                                : 'Please enter your details'}
+                            ? 'Link your account to get started'
+                            : 'Please enter your details'
+                            }
                         </p>
-                    </h1>
-
-                </div>
-            </header>
-            {user ? (
-                <div className='flex flex-col gap-4'>
-                    {/* PlaidLink */}
-                </div>
-            ) : (
-                <>
+                        </h1>
+                    </div>
+                    </header>
+                    {user ? (
+                        <div className="flex flex-col gap-4">
+                        <PlaidLink user={user} variant="primary" />
+                        </div>
+                    ): (
+                    <>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                             {type === 'sign-up' && (
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -54,47 +54,45 @@ export const signIn = async ({ email, password }: signInProps) => {
   }
 }
 
-// export const signUp = async ({ password, ...userData }: SignUpParams) => {
-export const signUp = async (userData: SignUpParams) => {
-  const { email, password, firstName, lastName } = userData;
+export const signUp = async ({ password, ...userData }: SignUpParams) => {
+  const { email, firstName, lastName } = userData;
 
-  // let newUserAccount;
+  let newUserAccount;
 
   try {
-    const { account } = await createAdminClient();
+    const { account, database } = await createAdminClient();
 
-     const newUserAccount= await account.create(
+    newUserAccount = await account.create(
       ID.unique(),
       email,
       password,
       `${firstName} ${lastName}`
     );
 
-    // if(!newUserAccount) throw new Error('Error creating user')
+    if(!newUserAccount) throw new Error('Error creating user')
 
-    // const dwollaCustomerUrl = await createDwollaCustomer({
-    //   ...userData,
-    //   type: 'personal'
-    // })
+    const dwollaCustomerUrl = await createDwollaCustomer({
+      ...userData,
+      type: 'personal'
+    })
 
-    // if(!dwollaCustomerUrl) throw new Error('Error creating Dwolla customer')
+    if(!dwollaCustomerUrl) throw new Error('Error creating Dwolla customer')
 
-    // const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
+    const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
 
-    // const newUser = await database.createDocument(
-    //   DATABASE_ID!,
-    //   USER_COLLECTION_ID!,
-    //   ID.unique(),
-    //   {
-    //     ...userData,
-    //     userId: newUserAccount.$id,
-    //     dwollaCustomerId,
-    //     dwollaCustomerUrl
-    //   }
-    // )
-    const session = await account.
-    createEmailPasswordSession(email, password);
-    // const session = await account.createEmailPasswordSession(email, password);
+    const newUser = await database.createDocument(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      ID.unique(),
+      {
+        ...userData,
+        userId: newUserAccount.$id,
+        dwollaCustomerId,
+        dwollaCustomerUrl
+      }
+    )
+
+    const session = await account.createEmailPasswordSession(email, password);
 
     cookies().set("appwrite-session", session.secret, {
       path: "/",
@@ -103,7 +101,7 @@ export const signUp = async (userData: SignUpParams) => {
       secure: true,
     });
 
-    return parseStringify(newUserAccount);
+    return parseStringify(newUser);
   } catch (error) {
     console.error('Error', error);
   }
@@ -112,9 +110,9 @@ export const signUp = async (userData: SignUpParams) => {
 export async function getLoggedInUser() {
   try {
     const { account } = await createSessionClient();
-    const user = await account.get();
+    const result = await account.get();
 
-    // const user = await getUserInfo({ userId: result.$id})
+    const user = await getUserInfo({ userId: result.$id})
 
     return parseStringify(user);
   } catch (error) {
